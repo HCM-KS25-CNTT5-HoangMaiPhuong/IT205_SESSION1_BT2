7f90b3980038b2a5b77bbd7d14c14d7b5afe5979
Mai Phuong <hoangmaiphuongtin@gmail.com>
2026-05-27 13:55:33 +0700

    bt2

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-print ('--HỆ THỐNG NHẬP CHỈ SỐ SINH TỒN--')
-name_patient = input('Nhập tên bệnh nhân:')
-weight = float(input('Nhập cân nặng bệnh nhân:'))
+print (" --- HỆ THỐNG NHẬP CHỈ SỐ SINH TỒN --- ")
 
-print('--- Kiểm tra dữ liruj lưu trữ ---')
-print('Bệnh nhân :', name_patient)
-print('Cân nặng đã nhập:',weight)
-print('Cảnh cáo- kiểu dữ liệu dạng lưu là:', type(weight))
+name_patient = input ("Nhập tên bệnh nhân : ")
+
+# Ép kiểu dữ liệu sang float
+weight = float(input ("Nhập cân nặng bệnh nhân : "))
+
+print (" --- KIỂM TRA DỮ LIỆU LƯU TRỮ --- ")
+
+print ("Bệnh nhân : ", name_patient)
+print ("Cân nặng đã nhập : ", weight)
+
+# Kiểm tra kiểu dữ liệu
+print ("CẢNH BÁO - Kiểu dữ liệu đang lưu là : ", type(weight))
 
 
 '''
